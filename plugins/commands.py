00eb5b81a294c6f10cc89b62f391619103cdaa4c
DeepSandhra <109024247+DeepSandhra@users.noreply.github.com>
2022-08-03 21:38:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙱𝙾𝚃 ?', url=f"https://t.me/movi2x")
+                InlineKeyboardButton('𝙱𝙾𝚃', url=f"http://t.me/MovieRequestGroup_rebot")
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
